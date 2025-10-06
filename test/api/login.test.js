d1fec05c89d2e6c235d79d2f0f7d154d99303e4e
IaraStevani <iarastevanialves@gmail.com>
2025-10-06 16:02:45 -0300

    FInalizando testes de transferência

--- test/api/login.test.js
+++ test/api/login.test.js
@@ -6,7 +6,7 @@ const postLogin = require('../../fixtures/postLogin.json');
 describe('Login', () => {
     describe('POST/users/login', () => {
         it('Deve retornar sucesso com 200 com um token em string quando usar credenciais válidas'), async () => {
-            const bodyLogin = {...postLogin};
+            const bodyLogin = { ...postLogin };
             const resposta = await request(process.env.BASE_URL_REST)
                 .post('/users/login')
                 .set('Content-Type', 'application/json')
@@ -16,4 +16,5 @@ describe('Login', () => {
             expect('resposta.body.token').to.be.a('string');
         }
     })
+
 })
--- test/api/transferencia.test.js
+++ test/api/transferencia.test.js
@@ -5,15 +5,16 @@ const { obterToken } = require('../../helpers/autenticacao');
 const postTransferencias = require('../../fixtures/postTransferencias.json');
 
 describe('Transfers', () => {
+    let token
+
+    beforeEach(async () => {
+        token = await obterToken('julio', '123456');
+    })
+
     describe('POST/transfers', () => {
-        let token
-        
-        beforeEach(async () => {
-             token = await obterToken('julio', '123456');
-        })
 
         it('Deve retornar sucesso com 201 quando a transferência for menor ou igual a R$5.000,00', async () => {
-            const bodyTransferencia = {...postTransferencias};
+            const bodyTransferencia = { ...postTransferencias };
 
             const resposta = await request(process.env.BASE_URL_REST)
                 .post('/transfers')
@@ -25,10 +26,10 @@ describe('Transfers', () => {
 
         it('Deve retornar falha com 400 quando a transferências for acima de R$ 5.000,00, só podem ser feitas para favorecido', async () => {
             it('Deve retornar sucesso com 201 quando a transferência for menor ou igual a R$5.000,00', async () => {
-                 const bodyTransferencia = {...postTransferencias};
-                 bodyTransferencia.value = 5000.01;
-                
-                const resposta = await request('http://localhost:3000')
+                const bodyTransferencia = { ...postTransferencias };
+                bodyTransferencia.value = 5000.01;
+
+                const resposta = await request(process.env.BASE_URL_REST)
                     .post('/transfers')
                     .set('Content-Type', 'application/json')
                     .set('Authorization', `Bearer ${token}`)
@@ -37,5 +38,15 @@ describe('Transfers', () => {
             })
         })
 
+        describe('GET/transfers', () => {
+            it('Deve retornar sucesso com 200 e dados iguais ao registro de transferências contidos no banco de dados', async () => {
+                const resposta = await request(process.env.BASE_URL_REST)
+                    .get('/transfers')
+                    .set('Authorization', `Bearer ${token}`)
+
+                    expect(resposta.status).to.equal(200);
+                    expect(resposta.body).to.be.an('array');
+            })
+        })
     })
 })
