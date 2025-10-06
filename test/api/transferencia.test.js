8f12a763c2956a26d9a0ee9ca568fccb89680d14
IaraStevani <iarastevanialves@gmail.com>
2025-10-06 08:06:41 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/api/transferencia.test.js
+++ test/api/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../../helpers/autenticacao');
+const postTransferencias = require('../../fixtures/postTransferencias.json');
 
 describe('Transfers', () => {
     describe('POST/transfers', () => {
@@ -12,29 +13,26 @@ describe('Transfers', () => {
         })
 
         it('Deve retornar sucesso com 201 quando a transferência for menor ou igual a R$5.000,00', async () => {
+            const bodyTransferencia = {...postTransferencias};
+
             const resposta = await request(process.env.BASE_URL_REST)
                 .post('/transfers')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    'from': 'julio',
-                    'to': 'priscila',
-                    'value': 11
-                })
+                .send(bodyTransferencia)
             expect(resposta.status).to.equal(201);
         })
 
         it('Deve retornar falha com 400 quando a transferências for acima de R$ 5.000,00, só podem ser feitas para favorecido', async () => {
             it('Deve retornar sucesso com 201 quando a transferência for menor ou igual a R$5.000,00', async () => {
+                 const bodyTransferencia = {...postTransferencias};
+                 bodyTransferencia.value = 5000.01;
+                
                 const resposta = await request('http://localhost:3000')
                     .post('/transfers')
                     .set('Content-Type', 'application/json')
                     .set('Authorization', `Bearer ${token}`)
-                    .send({
-                        'from': 'julio',
-                        'to': 'priscila',
-                        'value': 5000.01
-                    })
+                    .send(bodyTransferencia)
                 expect(resposta.status).to.equal(400);
             })
         })
