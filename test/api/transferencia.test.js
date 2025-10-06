1a05dade9badf5e7bb8e08c075596353249d4b52
IaraStevani <iarastevanialves@gmail.com>
2025-10-06 07:24:59 -0300

    Criei um helper de autenticação para obter o token

--- test/api/transferencia.test.js
+++ test/api/transferencia.test.js
@@ -1,18 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config();
+const { obterToken } = require('../../helpers/autenticacao');
 
 describe('Transfers', () => {
     describe('POST/transfers', () => {
         it('Deve retornar sucesso com 201 quando a transferência for menor ou igual a R$5.000,00', async () => {
-            const respostaLogin = await request(process.env.BASE_URL_REST)
-                .post('/users/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio',
-                    'password': '123456'
-                })
-
-            const token = respostaLogin.body.token;
+            const token = await obterToken('julio', '123456');
 
             const resposta = await request(process.env.BASE_URL_REST)
                 .post('/transfers')
@@ -28,15 +22,7 @@ describe('Transfers', () => {
 
         it('Deve retornar falha com 400 quando a transferências for acima de R$ 5.000,00, só podem ser feitas para favorecido', async () => {
             it('Deve retornar sucesso com 201 quando a transferência for menor ou igual a R$5.000,00', async () => {
-                const respostaLogin = await request('http://localhost:3000')
-                    .post('/users/login')
-                    .set('Content-Type', 'application/json')
-                    .send({
-                        'username': 'julio',
-                        'password': '123456'
-                    })
-
-                const token = respostaLogin.body.token;
+                const token = await obterToken('julio', '123456');
 
                 const resposta = await request('http://localhost:3000')
                     .post('/transfers')
@@ -47,7 +33,7 @@ describe('Transfers', () => {
                         'to': 'priscila',
                         'value': 5000.01
                     })
-                expect(resposta.status).to.equal(400);                
+                expect(resposta.status).to.equal(400);
             })
         })
 
