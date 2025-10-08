72cb7bf80f6fa02213ef15267d199bd15dabe215
IaraStevani <iarastevanialves@gmail.com>
2025-10-08 07:15:42 -0300

    Adicionando stages ao teste

--- test/performance/loginPerformance.test.js
+++ test/performance/loginPerformance.test.js
@@ -2,7 +2,12 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-    iterations: 100,
+   stages: [
+        { duration: '5s', target: 10 }, 
+        { duration: '20m', target: 10 }, 
+        { duration: '5s', target: 0 }, 
+    ],
+    
     thresholds: {        
         http_req_duration: ['p(95)<200', 'max<200'], 
         http_req_failed: ['rate<0.01'], 
