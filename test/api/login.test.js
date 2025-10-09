2dc895c19ed06c89fd8f171be855fb931a224a7c
IaraStevani <iarastevanialves@gmail.com>
2025-10-09 07:23:45 -0300

    Adicionando teste login invalido API

--- test/api/login.test.js
+++ test/api/login.test.js
@@ -2,19 +2,29 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const postLogin = require('../../fixtures/postLogin.json');
+const postLoginInvalido = require('../../fixtures/postLoginInvalido.json');
 
 describe('Login', () => {
     describe('POST/users/login', () => {
-        it('Deve retornar sucesso com 200 com um token em string quando usar credenciais válidas'), async () => {
+        it('Deve retornar sucesso com 200 com um token em string quando usar credenciais válidas', async () => {
             const bodyLogin = { ...postLogin };
             const resposta = await request(process.env.BASE_URL_REST)
                 .post('/users/login')
                 .set('Content-Type', 'application/json')
-                .send(postLogin)
+                .send(postLogin);
 
             expect(resposta.status).to.equal(200);
-            expect('resposta.body.token').to.be.a('string');
-        }
-    })
+            expect(resposta.body.token).to.be.a('string');
+        });
 
-})
+        it('Deve retornar falha com 400 quando usar credenciais inválidas', async () => {
+            const bodyLoginInvalido = { ...postLoginInvalido }
+            const resposta = await request(process.env.BASE_URL_REST)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(postLoginInvalido);
+
+            expect(resposta.status).to.equal(400);
+        });
+    });
+});
