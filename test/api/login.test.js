1ff2e823cb84763a1bacc4f8b1307767d0681238
IaraStevani <iarastevanialves@gmail.com>
2025-10-09 07:14:33 -0300

    Ajustando teste

--- test/api/login.test.js
+++ test/api/login.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const postLogin = require('../../fixtures/postLogin.json');
+const postLoginInvalido = require('../../fixtures/postLoginInvalido.json');
 
 describe('Login', () => {
     describe('POST/users/login', () => {
@@ -14,6 +15,19 @@ describe('Login', () => {
 
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
-        });        
+        });
+
+        it('Deve retornar falha com 400 quando usar credenciais inválidas', async () => {
+            const bodyLoginInvalido = {...postLoginInvalido}
+            const resposta = await request(process.env.BASE_URL_REST)
+                .post('/users/login')
+                .set('Content-Type', 'application/json')
+                .send(postLoginInvalido);
+
+            console.log(resposta.body);
+            console.log(resposta.status);
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body.message).to.equal('Usuário ou senha inválidos');
+        });
     });
 });
